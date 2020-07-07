94eda0b50f848081641769d04b87f9e22abef7a2
mrdoowan <stevenduan888@gmail.com>
2020-07-06 22:08:59 -0400

    [vB.3]

--- client/src/components/Helmet/HelmetComponent.js
+++ client/src/components/Helmet/HelmetComponent.js
@@ -7,14 +7,18 @@ export default function HelmetComponent({ title, description, image, url }) {
         <Helmet>
             {(title) ? <title>{`${title} - LHG Leagues`}</title> : null}
             {(description) ? <meta name="description" content={description} /> : null}
-            {(title) ? <meta name="twitter:title" content={title} /> : null}
-            {(description) ? <meta name="twitter:description" content={description} /> : null}
-            {(image) ? <meta name="twitter:image" content={image} /> : null}
+            {/* Og - Open Graph */}
             {(title) ? <meta property="og:title" content={title} /> : null}
             {(description) ? <meta property="og:description" content={description} /> : null}
             {(image) ? <meta property="og:image" content={image} /> : null}
             {(url) ? <meta property="og:url" content={url} /> : null}
             <meta property="og:site_name" content="LHG Competitive Leagues" />
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:type" content="website" />
+            {/* Twitter */}
+            {(title) ? <meta name="twitter:title" content={title} /> : null}
+            {(description) ? <meta name="twitter:description" content={description} /> : null}
+            {(image) ? <meta name="twitter:image" content={image} /> : null}
         </Helmet>
     );
 }
