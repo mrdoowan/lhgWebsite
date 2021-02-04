18de12baf3a0d530e7f882943f87061f7aa2d6c4
mrdoowan <stevenduan888@gmail.com>
2021-02-04 18:15:23 -0500

    added npm node-schedule

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import profileV1Routes from './routes/apiV1/profileRoutes.js';
 import teamV1Routes from './routes/apiV1/teamRoutes.js';
 import matchV1Routes from './routes/apiV1/matchRoutes.js';
 import staffV1Routes from './routes/apiV1/staffRoutes.js';
+import { checkRdsStatus, stopRdsInstance } from './functions/apiV1/dependencies/awsRdsHelper';
 
 /*  Declaring npm modules */
 dotenv.config();
@@ -16,6 +17,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+const schedule = require('node-schedule');
 
 // Configure express
 app.use(bodyParser.json());
@@ -41,6 +43,11 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+// Check if the MySQL Db is "Available" every hour. If so, stop the instance.
+schedule.scheduleJob('00 * * * *', function(){
+    console.log(`I'm working!`);
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Stats server started on port ${port}`));
