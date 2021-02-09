1de92a2b5870e45556612e6add6ac9f4e3423016
mrdoowan <stevenduan888@gmail.com>
2021-02-09 11:06:51 -0500

    match setup list page complete

--- client/src/components/Match/MatchSetupList.js
+++ client/src/components/Match/MatchSetupList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     Paper,
@@ -16,18 +17,36 @@ const useStyles = makeStyles((theme) => ({
         color: theme.palette.text.primary,
         background: '#A9A9A9',
     },
+    row: {
+        padding: theme.spacing(2),
+        fontWeight: 'bold',
+    },
+    title: {
+        padding: theme.spacing(2),
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+        fontSize: 'xx-large',
+    },
+    link: {
+        color: 'darkBlue',
+    },
 }));
 
 export default function MatchSetupList({ setupListData }) {
     const classes = useStyles();
 
-    console.log(setupListData);
-
     return (<div>
         <Grid container spacing={3}>
             <Grid item xs={12}>
                 <Paper className={classes.paper}>
-                    
+                    <h1 className={classes.title}>
+                        Match Setup Ids List
+                    </h1>
+                    {setupListData.map((matchId, idx) => (
+                        <span className={classes.row} key={`matchSetup${idx}Link`}>
+                            <Link to={`/match/${matchId}/setup`} className={classes.link}>{matchId}</Link> <br />
+                        </span>
+                    ))}
                 </Paper>
             </Grid>
         </Grid>
