2d99f1127636be935fd5c7098497e5fdc9e81ddd
mrdoowan <stevenduan888@gmail.com>
2021-02-12 20:49:15 -0500

    functionalized validateFields

--- functions/apiV1/matchSubmit.js
+++ functions/apiV1/matchSubmit.js
@@ -2,12 +2,14 @@
 
 /*  Import dependency modules */
 import { ChampById } from '../../client/src/static/ChampById';
+import { checkRdsStatus } from './dependencies/awsRdsHelper';
 import { getProfilePIdByName } from './profileData';
+import { getTeamPIdByName } from './teamData';
+import { AWS_RDS_STATUS } from '../../services/Constants';
+const BLUE = 'Blue';
+const RED = 'Red';
 
 export const submitMatchSetup = (id) => {
-    const BLUE = 'Blue';
-    const RED = 'Red';
-
     return new Promise(async (resolve, reject) => {
         try {
             const matchDbObject = await dynamoDbGetItem('Matches', 'MatchPId', id);
@@ -25,53 +27,85 @@ export const submitMatchSetup = (id) => {
                 return;
             }
 
-            const setupDbObject = matchDbObject['Setup'];
+            // Check validateList and resolve with validation errors
+            const validateList = await validateSetupFormFields(matchDbObject.Setup.Teams);
+            if (validateList.length > 0) {
+                resolve({
+                    error: 'Form fields from Match Setup are not valid.',
+                    validateMessages: validateList,
+                });
+                return;
+            }
+            
+            // Process object into databases
+
+        }
+        catch (error) {
+            console.error(error); reject(error);
+        }
+    });
+}
+
+function validateSetupFormFields(setupTeamsDbObject) {
+    return new Promise(async (resolve, reject) => {
+        try {
             const validateList = [];
+
             // Check all the bans that they are actual champIds
             const checkBans = (color, banList) => {
-                banList.forEach((banId, idx) => {
+                for (let i = 0; i < banList.length; ++i) {
+                    const banId = banList[i];
                     if (!(banId in ChampById)) {
                         validateList.push(
                             `${color} Team Bans of Id '${banId}' at index ${idx} is invalid.`
                         );
                     }
-                });
+                }
             }
-            checkBans(BLUE, setupDbObject.Teams.BlueTeam.Bans);
-            checkBans(RED, setupDbObject.Teams.RedTeam.Bans);
+            checkBans(BLUE, setupTeamsDbObject.BlueTeam.Bans);
+            checkBans(RED, setupTeamsDbObject.RedTeam.Bans);
             // Check if all profileNames exist in DynamoDb
-            const checkProfiles = (color, playerList) => {
-                playerList.forEach((player) => {
-                    const profilePId = await getProfilePIdByName(player.ProfileName);
+            const checkProfiles = async (color, playerList) => {
+                for (let i = 0; i < playerList.length; ++i) {
+                    const playerObject = playerList[i];
+                    const profilePId = await getProfilePIdByName(playerObject.ProfileName);
                     if (!profilePId) {
                         validateList.push(
-                            `${color} Team Profile Name '${player.ProfileName}' does not exist in the database.`
+                            `${color} Team Profile '${player.ProfileName}' Name does not exist in database.`
                         );
                     }
                     else {
-                        
+                        setupTeamsDbObject[`${color}Team`].Players[idx].ProfilePId = profilePId;
                     }
-                });
+                }
             }
-            checkProfiles(BLUE, setupDbObject.Teams.BlueTeam.Players);
-            checkProfiles(RED, setupDbObject.Teams.RedTeam.Players);
+            checkProfiles(BLUE, setupTeamsDbObject.BlueTeam.Players);
+            checkProfiles(RED, setupTeamsDbObject.RedTeam.Players);
             // Check if both teamNames exist in DynamoDb
-            const checkTeamName = (teamName) => {
-                
+            const checkTeamName = async (color, teamName) => {
+                const teamPId = await getTeamPIdByName(teamName);
+                if (!teamPId) {
+                    validateList.push(
+                        `${color} Team '${teamName}' Name does not exist in database.`
+                    );
+                }
+                else {
+                    setupTeamsDbObject[`${color}Team`].TeamPId = teamPId;
+                }
+            }
+            checkTeamName(BLUE, setupTeamsDbObject.BlueTeam.TeamName);
+            checkTeamName(RED, setupTeamsDbObject.RedTeam.TeamName);
+            // Check if the MySQL Db is available
+            if ((await checkRdsStatus()) !== AWS_RDS_STATUS.AVAILABLE) {
+                validateList.push(`
+                    MySQL Database is inactive. Start it.
+                `);
             }
 
-            // Check if the MySQL Db is available
-
-            // Check validateList and resolve with validation errors
-
-            // Transform data into object
-
-
-
-            // Process object into databases
+            resolve(validateList);
         }
-        catch (error) {
-            console.error(error); reject(error);
+        catch (err) {
+            reject(err);
         }
     });
 }
