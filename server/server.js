6d94da23997e625da2d76f49e9682a0128bdd466
mrdoowan <stevenduan888@gmail.com>
2021-03-01 19:02:09 -0500

    process.env.TEST_DB === 'false' logic instead to default to test Dbs

--- server/server.js
+++ server/server.js
@@ -79,4 +79,4 @@ schedule.scheduleJob(rule, function(){
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Stats server started on port ${port}`));
-console.log((process.env.TEST_DB === 'true') ? "Connected to DB Test endpoints." : "Connected to DB Production endpoints!");
+console.log((process.env.TEST_DB === 'false') ? "Connected to DB Production endpoints!" : "Connected to DB Test endpoints.");
