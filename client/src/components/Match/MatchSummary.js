d36e9bdf61bbf8714ce589adc33125fd7e13f874
Charlie Evans <charlie@project-owl.com>
2021-03-30 22:54:45 -0500

    Added first draft of gold diff graph; added profile links
    Signed-off-by: Charlie Evans <charlie@project-owl.com>

--- client/src/components/Match/MatchSummary.js
+++ client/src/components/Match/MatchSummary.js
@@ -9,18 +9,33 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import {
+    Chart,
+    Series,
+    ArgumentAxis,
+    CommonSeriesSettings,
+    Export,
+    Legend,
+    Margin,
+    Title,
+    Subtitle,
+    Tooltip,
+    CommonAxisSettings
+  } from 'devextreme-react/chart';
 
 const StyledTableCellBlue = withStyles((theme) => ({
     head: {
         backgroundColor: "#1241CE",
-        color: theme.palette.common.white
+        color: theme.palette.common.white,
+        fontSize: 20
     }
 }))(TableCell);
 
 const StyledTableCellRed = withStyles((theme) => ({
     head: {
         backgroundColor: "#CB2C31",
-        color: theme.palette.common.white
+        color: theme.palette.common.white,
+        fontSize: 20
     }
 }))(TableCell);
 
@@ -45,6 +60,32 @@ export default function MatchSummary({ match }) {
     const classes = useStyles();
     console.log(match);
 
+    // used for the gold data
+    // the graphs are typically a gold difference at a point in time
+    // so our x-axis will be the minute and the y-axis the blue-red calc
+    let goldChartData = [];
+    let maxGoldDiff = 0;
+
+    for (const i in match.Timeline) {
+        let obj = {
+            minute: match.Timeline[i].MinuteStamp,
+            goldDiff: (match.Timeline[i].BlueTeamGold - match.Timeline[i].RedTeamGold)
+        };
+
+        if (Math.abs(obj.goldDiff) > maxGoldDiff) {
+            maxGoldDiff = Math.abs(obj.goldDiff);
+        }
+
+        goldChartData.push(obj);
+    }
+
+    let valueAxisSettings = {
+        visualRange: {
+            startValue: maxGoldDiff * -1 * 1.02,
+            endValue: maxGoldDiff * 1.02
+        }
+    };
+
     return (
         <div>
             <Grid container spacing={3}>
@@ -56,14 +97,14 @@ export default function MatchSummary({ match }) {
                                 <StyledTableCellBlue>Player</StyledTableCellBlue>
                                 <StyledTableCellBlue></StyledTableCellBlue>
                                 <StyledTableCellBlue></StyledTableCellBlue>
-                                <StyledTableCellBlue>KDA</StyledTableCellBlue>
+                                <StyledTableCellBlue>K/D/A</StyledTableCellBlue>
                                 <StyledTableCellBlue>CS</StyledTableCellBlue>
                             </TableRow>
                             </TableHead>
                             <TableBody>
                                 {Object.entries(match.Teams['100'].Players).map(([playerNum, player]) => (
                                 <TableRow key={playerNum}>
-                                    <TableCell>{player.ProfileName}</TableCell>
+                                    <TableCell><a href={`/profile/${player.ProfileName}/games/${match.SeasonShortName}`}>{player.ProfileName}</a></TableCell>
                                     <TableCell></TableCell>
                                     <TableCell></TableCell>
                                     <TableCell>{player.Kills}/{player.Deaths}/{player.Assists}</TableCell>
@@ -83,14 +124,14 @@ export default function MatchSummary({ match }) {
                                 <StyledTableCellRed>Player</StyledTableCellRed>
                                 <StyledTableCellRed></StyledTableCellRed>
                                 <StyledTableCellRed></StyledTableCellRed>
-                                <StyledTableCellRed>KDA</StyledTableCellRed>
+                                <StyledTableCellRed>K/D/A</StyledTableCellRed>
                                 <StyledTableCellRed>CS</StyledTableCellRed>
                             </TableRow>
                             </TableHead>
                             <TableBody>
                                 {Object.entries(match.Teams['200'].Players).map(([playerNum, player]) => (
                                 <TableRow key={playerNum}>
-                                    <TableCell>{player.ProfileName}</TableCell>
+                                    <TableCell><a href={`/profile/${player.ProfileName}/games/${match.SeasonShortName}`}>{player.ProfileName}</a></TableCell>
                                     <TableCell></TableCell>
                                     <TableCell></TableCell>
                                     <TableCell>{player.Kills}/{player.Deaths}/{player.Assists}</TableCell>
@@ -101,6 +142,14 @@ export default function MatchSummary({ match }) {
                         </Table>
                     </TableContainer>
                 </Grid>
+            </Grid>
+            <Grid item xs={12}>
+                <Chart palette="DarkViolet" dataSource={goldChartData} valueAxis={valueAxisSettings}>
+                    <CommonSeriesSettings argumentField="minute" type="line" />
+                    <Series key="goldDiff" valueField="goldDiff" />
+                    <Title text="Gold Difference Over Time"></Title>
+                    <Tooltip enabled={true} />
+                </Chart>
             </Grid>
         </div>
     )
