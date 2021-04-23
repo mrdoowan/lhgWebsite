feb957dbe3015e10f0e37ee6ca8c3217cb01fb0c
Charlie Evans <charlie@project-owl.com>
2021-04-22 20:58:28 -0500

    Added more graphs and tables to supply a lot more data
    Signed-off-by: Charlie Evans <charlie@project-owl.com>

--- client/src/components/Match/MatchSummary.js
+++ client/src/components/Match/MatchSummary.js
@@ -22,7 +22,9 @@ import {
     Subtitle,
     Tooltip,
     CommonAxisSettings,
-    Font
+    Font,
+    Label,
+    Format
   } from 'devextreme-react/chart';
 
 import ChampionSquare from '../ChampionSquare';
@@ -97,7 +99,126 @@ export default function MatchSummary({ match }) {
         }
     };
 
-    let winningTeam = match.Teams['100'].Win === true ? match.Teams['100'].TeamName : match.Teams['200'].TeamName;
+    let blueTeamName = match.Teams['100'].TeamName;
+    let redTeamName = match.Teams['200'].TeamName;
+
+    let winningTeam = match.Teams['100'].Win === true ? blueTeamName : redTeamName;
+
+    // Vision
+    let visionData = [];
+
+    for (const t in match.Teams) {
+        const item = match.Teams[t];
+        const obj = {
+            team: item.TeamName,
+            wardsPlaced: item.TeamWardsPlaced,
+            wardsCleared: item.TeamWardsCleared
+        };
+
+        visionData.push(obj);
+    }
+
+    // Damage Distribution
+    let damageDistribution = [
+        {
+            lane: 'Top',
+            blueDamage: parseFloat(match.Teams['100'].Players['1'].DamageDealtPct) * 100,
+            redDamage: parseFloat(match.Teams['200'].Players['6'].DamageDealtPct) * 100
+        },
+        {
+            lane: 'Jungle',
+            blueDamage: parseFloat(match.Teams['100'].Players['2'].DamageDealtPct) * 100,
+            redDamage: parseFloat(match.Teams['200'].Players['7'].DamageDealtPct) * 100
+        },
+        {
+            lane: 'Mid',
+            blueDamage: parseFloat(match.Teams['100'].Players['3'].DamageDealtPct) * 100,
+            redDamage: parseFloat(match.Teams['200'].Players['8'].DamageDealtPct) * 100
+        },
+        {
+            lane: 'ADC',
+            blueDamage: parseFloat(match.Teams['100'].Players['4'].DamageDealtPct) * 100,
+            redDamage: parseFloat(match.Teams['200'].Players['9'].DamageDealtPct) * 100
+        },
+        {
+            lane: 'Support',
+            blueDamage: parseFloat(match.Teams['100'].Players['5'].DamageDealtPct) * 100,
+            redDamage: parseFloat(match.Teams['200'].Players['10'].DamageDealtPct) * 100
+        },
+    ];
+
+    // Gold Distribution
+    let goldDistribution = [
+        {
+            lane: 'Top',
+            blue: parseFloat(match.Teams['100'].Players['1'].GoldPct) * 100,
+            red: parseFloat(match.Teams['200'].Players['6'].GoldPct) * 100
+        },
+        {
+            lane: 'Jungle',
+            blue: parseFloat(match.Teams['100'].Players['2'].GoldPct) * 100,
+            red: parseFloat(match.Teams['200'].Players['7'].GoldPct) * 100
+        },
+        {
+            lane: 'Mid',
+            blue: parseFloat(match.Teams['100'].Players['3'].GoldPct) * 100,
+            red: parseFloat(match.Teams['200'].Players['8'].GoldPct) * 100
+        },
+        {
+            lane: 'ADC',
+            blue: parseFloat(match.Teams['100'].Players['4'].GoldPct) * 100,
+            red: parseFloat(match.Teams['200'].Players['9'].GoldPct) * 100
+        },
+        {
+            lane: 'Support',
+            blue: parseFloat(match.Teams['100'].Players['5'].GoldPct) * 100,
+            red: parseFloat(match.Teams['200'].Players['10'].GoldPct) * 100
+        },
+    ];
+
+    let otherStats = [
+        {
+            name: 'End Gold',
+            blue: match.Teams['100'].TeamGold,
+            red: match.Teams['200'].TeamGold,
+        },
+        {
+            name: 'Total Kills',
+            blue: match.Teams['100'].TeamKills,
+            red: match.Teams['200'].TeamKills,
+        },
+        {
+            name: 'Total Deaths',
+            blue: match.Teams['100'].TeamDeaths,
+            red: match.Teams['200'].TeamDeaths,
+        },
+        {
+            name: 'Total Assists',
+            blue: match.Teams['100'].TeamAssists,
+            red: match.Teams['200'].TeamAssists,
+        },
+        {
+            name: 'Towers',
+            blue: match.Teams['100'].Towers,
+            red: match.Teams['200'].Towers,
+        },
+        {
+            name: 'Inhibitors',
+            blue: match.Teams['100'].Inhibitors,
+            red: match.Teams['200'].Inhibitors,
+        },
+        {
+            name: 'Vision Score',
+            blue: match.Teams['100'].TeamVisionScore,
+            red: match.Teams['200'].TeamVisionScore,
+        },
+        {
+            name: 'Damage Dealt',
+            blue: match.Teams['100'].TeamDamageDealt,
+            red: match.Teams['200'].TeamDamageDealt,
+        },
+    ];
+    
 
     return (
         <div>
@@ -178,8 +299,88 @@ export default function MatchSummary({ match }) {
                         </Table>
                     </TableContainer>
                 </Grid>
+
+                <Grid item xs={6}>
+                    <Paper elevation={3}>
+                        <Chart id="wardsChart" dataSource={visionData}>
+                            <CommonSeriesSettings argumentField="state" type="bar" hoverMode="allArgumentPoints" selectionMode="allArgumentPoints">
+                                <Label visible={true}>
+                                    <Format type="fixedPoint" precision={0} />
+                                </Label>
+                            </CommonSeriesSettings>
+                            <Title text="Vision">
+                                <Font color="black"></Font>
+                            </Title>
+                            <Series argumentField="team" valueField="wardsPlaced" name="Wards Placed" />
+                            <Series argumentField="team" valueField="wardsCleared" name="Wards Cleared" />
+                            <Legend verticalAlignment="bottom" horizontalAlignment="center"></Legend>
+                        </Chart>
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <Paper elevation={3}>
+                        <Chart id="damagePercentChart" dataSource={damageDistribution}>
+                            <CommonSeriesSettings argumentField="state" type="bar" hoverMode="allArgumentPoints" selectionMode="allArgumentPoints">
+                                <Label visible={true}>
+                                    <Format type="fixedPoint" precision={0} />
+                                </Label>
+                            </CommonSeriesSettings>
+                            <Title text="Damage Percentages by Role">
+                                <Font color="black"></Font>
+                            </Title>
+                            <Series argumentField="lane" valueField="blueDamage" name={blueTeamName} />
+                            <Series argumentField="lane" valueField="redDamage" name={redTeamName} />
+                            <Legend verticalAlignment="bottom" horizontalAlignment="center"></Legend>
+                        </Chart>
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <Paper elevation={3}>
+                        <Chart id="goldPercentChart" dataSource={goldDistribution}>
+                            <CommonSeriesSettings argumentField="state" type="bar" hoverMode="allArgumentPoints" selectionMode="allArgumentPoints">
+                                <Label visible={true}>
+                                    <Format type="fixedPoint" precision={0} />
+                                </Label>
+                            </CommonSeriesSettings>
+                            <Title text="Gold Percentages by Role">
+                                <Font color="black"></Font>
+                            </Title>
+                            <Series argumentField="lane" valueField="blue" name={blueTeamName} />
+                            <Series argumentField="lane" valueField="red" name={redTeamName} />
+                            <Legend verticalAlignment="bottom" horizontalAlignment="center"></Legend>
+                        </Chart>
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>Stats</TableCell>
+                                <TableCell>{blueTeamName}</TableCell>
+                                <TableCell>{redTeamName}</TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    otherStats.map(item =>
+                                        <TableRow key={item.name}>
+                                            <TableCell>{item.name}</TableCell>
+                                            <TableCell>{item.blue}</TableCell>
+                                            <TableCell>{item.red}</TableCell>
+                                        </TableRow>
+                                    )
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
             </Grid>
-            <Box paddingTop={3}>
+            {/* Going to move this to another tab soon */}
+            {/* <Box paddingTop={3}>
                 <Paper elevation={3}>
                     <Chart dataSource={goldChartData} argumentAxis={argumentAxisSettings} valueAxis={valueAxisSettings}>
                         <CommonSeriesSettings argumentField="minute" type="line" />
@@ -191,7 +392,7 @@ export default function MatchSummary({ match }) {
                         <Legend visible={false}></Legend>
                     </Chart>
                 </Paper>
-            </Box>
+            </Box> */}
         </div>
     )
 }
